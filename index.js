ef24e9ea5d4e1f8ebc2eae07f0ca4a036f135303
Aljoboyer <tanveeazmain@gmail.com>
2022-02-19 16:33:49 +0600

    fixed

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ const { v4: uuidv4 } = require('uuid');
 const port = process.env.PORT || 5000;
 
 //middleware 
-app.use(cors());
-app.use(express.json());
+app.use(cors()); 
+app.use(express.json({limit: '50mb'}));
 app.use(fileUpload());
+app.use(express.urlencoded({limit: '50mb'}));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obwta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -596,11 +597,15 @@ async function run() {
         });
  
         app.post('/addmissionpayment', async (req, res) => {
-        
-            const admissiondata = req.body.admissiondata
-            
+            const datas = req.body
+            const front = req.files.img.data;
+             
+            const encodedpic = front.toString('base64');
+            const img = Buffer.from(encodedpic, 'base64');
+            const admissiondata = {...datas, img}
+
             const productInfo = {
-                total_amount: req.body.total_amount,
+                total_amount: req.body.totalAmount,
                 currency: 'BDT',
                 tran_id: uuidv4(),
                 success_url: 'http://localhost:5000/successes',
